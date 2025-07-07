8f0aca5a412664fab53c1d3a2ca646d4b273cb8e
manas9853 <manas9853@users.noreply.github.com>
2025-07-07 11:10:05 +0000

    deploy: e7864c779b8f374217d47c88a379ae284b490056

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751886451',resources: [
+    cacheName: 'chirpy-1751886604',resources: [
       '/thebalancedbrief/assets/css/jekyll-theme-chirpy.css',
       '/thebalancedbrief/',
       
