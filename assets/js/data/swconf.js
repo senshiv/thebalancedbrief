f149c9d587a3f1c5e93cb3ab31986f297d4c30c2
senshiv <senshiv@users.noreply.github.com>
2025-07-07 11:41:05 +0000

    deploy: 298bc0765f7c9d72e52cd12f6a52a7ff63cad11a

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1751886928',resources: [
+    cacheName: 'chirpy-1751888464',resources: [
       '/thebalancedbrief/assets/css/jekyll-theme-chirpy.css',
       '/thebalancedbrief/',
       
